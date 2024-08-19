19a991d2ea1614ae6dd9e692d9b7e51e4fcebf51
ghosiajaved <ghosia.javed15@gmail.com>
2024-08-19 19:32:24 +0500

    Http server with single endpoint added

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+import fs from 'fs';  // Correct import for fs module
+import { createServer } from 'http';
 
+const myServer = createServer((req, res) => {
+    
+  const log = `${Date.now()}: ${req.url} New Req Received\n`;
 
-console.log("NODE JS PROJECT SETUP COMPLETED");
+  fs.appendFile('log.txt', log, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      res.end('Error occurred');
+      return;
+    }
+    res.end('Hello From Server Again');
+  });
+});
+
+myServer.listen(8000, () => console.log('Server Started!'));
+
+// console.log("NODE JS PROJECT SETUP COMPLETED");
